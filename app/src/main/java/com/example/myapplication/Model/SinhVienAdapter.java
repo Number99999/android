725163973013213772e1de31e3e024e647814001
Number99999 <nguyenngocduong.ecorphd@gmail.com>
2023-06-22 02:19:57 +0700

    new

--- app/src/main/java/com/example/myapplication/Model/SinhVienAdapter.java
+++ app/src/main/java/com/example/myapplication/Model/SinhVienAdapter.java
@@ -1,20 +1,26 @@
 package com.example.myapplication.Model;
 
+import static android.os.Build.VERSION_CODES.R;
+import com.example.myapplication.R;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
 public class SinhVienAdapter extends BaseAdapter {
     private Context context;
-    private int id;
+    private int layout;
     private List<SinhVien> listSV;
 
-    public SinhVienAdapter(Context context, int id, List<SinhVien> listSV) {
+    public SinhVienAdapter(Context context, int layout, List<SinhVien> listSV) {
         this.context = context;
-        this.id = id;
+        this.layout = layout;
         this.listSV = listSV;
     }
 
@@ -35,6 +41,15 @@ public class SinhVienAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = inflater.inflate(this.layout, null);
+
+        TextView txtId =(TextView) convertView.findViewById(R.id.idSV);
+        TextView txtName = (TextView) convertView.findViewById(R.id.name);
+        TextView txtYearBirth = (TextView) convertView.findViewById(R.id.yearBirth);
+        TextView txtHomeTown =  (TextView) convertView.findViewById(R.id.homeTown);
+        TextView txtYear = (TextView) convertView.findViewById(R.id.year);
+
         return null;
     }
 }
